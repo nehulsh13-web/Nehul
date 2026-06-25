1235101b05eb069a8a34a87b0b0491cd7202a4a1
nehulsh13-web <nehulsh13@gmail.com>
2026-06-25 14:33:43 +0800

    Data Types

---
+++ datatype.py
@@ -0,0 +1,8 @@
+x= 24
+y= "Nehul"
+z= 26.18
+a= True 
+print ("The data type of x is", type (x))
+print ("The data type of y is", type (y))
+print ("The data type of z is", type (z))
+print ("The data type of a is", type (a))
