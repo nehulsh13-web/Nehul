99d2e61e06fd683846973aa3e9dab3c6722a1a73
nehulsh13-web <nehulsh13@gmail.com>
2026-06-25 14:55:40 +0800

    Type casting

---
+++ typecasting.py
@@ -0,0 +1,8 @@
+marks= 30
+grams= 20.87
+School= "Morrison"
+print ("Convert data type of marks to float", float(marks))
+print ("The data type of marks is", type(marks))
+grams1= int(grams)
+print ("Convert data type of grams to integer",grams1)
+print ("The data type of grams is now",type(grams1))
