93cad678b756b568b96615d712a036768eeddc46
nehulsh13-web <nehulsh13@gmail.com>
2026-08-23 11:56:19 +0800

    Multiple Exeptions

---
+++ multipleexeptions.py
@@ -0,0 +1,19 @@
+try:
+    num1,num2 = eval(input("Enter two numbers, seperate by a comma : "))
+    result = num1 / num2
+    print("Result is", result)
+
+except ZeroDivisionError:
+    print("Division by zero is error !!")
+
+except SyntaxError:
+    print("Comma is missing. Enter numbers seperated by comma, like this 1, 2")
+
+except:
+    print("Wrong input")
+
+else:
+    print("No exeption")
+
+finally:
+    print("This will execute no matter what")
