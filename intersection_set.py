2db0d7604efdd30ea97ddbbbb4086f74e86b0846
nehulsh13-web <nehulsh13@gmail.com>
2026-09-19 12:59:37 +0800

    intersection Sets

---
+++ intersection_set.py
@@ -0,0 +1,11 @@
+set1 = {"Green","Blue", "Orange", "Red","Yellow"}
+set2 = {"Dark Green", "Sky Blue", "Maroon","Blue"}
+print("Original Sets: ")
+print(set1)
+print(set2)
+print("Intersection of both sets :")
+set3 = set1.intersection(set2)
+print(set3)
+set4 = set1.union(set2)
+print("Union of both sets :")
+print(set4)
