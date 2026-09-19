2311647c2942acb3af587e249c8e8e18eef5adf8
nehulsh13-web <nehulsh13@gmail.com>
2026-09-20 01:01:19 +0800

    Symmetric Diffrences

---
+++ symmetric_difference.py
@@ -0,0 +1,7 @@
+set1_A = {"blue", "green"}
+set2_A = {"blue", "yellow"}
+print(set1_A.symmetric_difference(set2_A))
+
+set1_B = {1, 2, 3, 4, 5}
+set2_B = {1, 5, 6, 7, 8, 9}
+print(set1_B.symmetric_difference(set2_B))
