da7660e78f2f92a2436e75117190c60d0b572e2a
nehulsh13-web <nehulsh13@gmail.com>
2026-09-20 00:54:38 +0800

    Tuple Product

---
+++ tuple_product.py
@@ -0,0 +1,13 @@
+import math
+
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+
+product_tup1 = math.prod(tup1)
+product_tup2 = math.prod(tup2)
+
+final_product = product_tup1 * product_tup2
+
+print(f"Product of tup1: {product_tup1}")
+print(f"Product of tup2: {product_tup2}")
+print(f"Total product of all numbers: {final_product}")
